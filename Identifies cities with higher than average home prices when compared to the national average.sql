deb7046d01d99ea022ed1e560cda5b92e6f77ff7
Todor <tvelichkovt@gmail.com>
2021-04-29 18:27:18 +0300

    SaaS_Growth_Metrics_SQL

---
+++ Identifies cities with higher than average home prices when compared to the national average.sql
@@ -0,0 +1,16 @@
+# Write a query that identifies cities with higher than average home prices when compared to the national average. 
+	# Output the city names.
+
+use classicmodels;
+select * from customers;
+
+select country, sum(creditLimit) HOMEprices from customers group by 1 having(HOMEprices) > 305718 order by 1;
+
+drop view db;
+create view db as select country, sum(creditLimit) HOMEprices from customers group by 1 order by 1;
+select * from db;
+select avg(HOMEprices) from db; # 305718
+
+select * from db group by 1 having(HOMEprices >= (select avg(HOMEprices) from db)) order by 1 desc;
+
+select country, sum(creditLimit) HOMEprices from customers group by 1 having(HOMEprices) > 305718 order by 1;
