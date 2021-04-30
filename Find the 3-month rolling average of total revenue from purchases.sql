361e49a2587c561a075c2ceaf30d14b0324df59b
Todor <tvelichkovt@gmail.com>
2021-04-30 12:31:45 +0300

    SaaS_Growth_Metrics_SQL

--- Find the 3-month rolling average of total revenue from purchases.sql
+++ Find the 3-month rolling average of total revenue from purchases.sql
@@ -1,5 +1,3 @@
-# https://platform.stratascratch.com/coding-question?python=&id=10314
-
 # Find the 3-month rolling average of total revenue from purchases given a table with users, their purchase amount, and date purchased.
 	# Do not include returns which are represented by negative purchase values. 
 		# Output the month and 3-month rolling average of revenue, sorted from earliest month to latest month.
@@ -7,8 +5,10 @@
 				# The first two months will not be a true 3-month rolling average since we are not given data from last year.
 
 use classicmodels;
-select * from payments;
+select * from payments order by paymentDate asc;
 
-select *, 
-avg(amount) over (order by paymentDate ASC ROWS 2 PRECEDING) Three_Month_RolAVG
-from payments;
+drop view db;
+create view db as select year(paymentDate) YR, month(paymentDate) MO, sum(amount) REV from payments group by 1, 2 order by 1, 2;
+select db.*, avg(REV) over (order by YR, MO asc rows 2 preceding) from db;
+
+select *, avg(REV) over (order by YR, MO asc rows 2 preceding) 3_MO_EOL_AVG from(select year(paymentDate) YR, month(paymentDate) MO, sum(amount) REV from payments where amount >= 0 group by 1, 2 order by 1, 2) subquery_derived_table;
