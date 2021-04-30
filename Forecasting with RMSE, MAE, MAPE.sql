f90ebf60cc5d846531c4bbf19d6f32d88922cf35
Todor <tvelichkovt@gmail.com>
2021-04-30 14:54:03 +0300

    SaaS_Growth_Metrics_SQL

---
+++ Forecasting with RMSE, MAE, MAPE.sql
@@ -0,0 +1,18 @@
+# Root Mean Squared Error (RMSE)
+	# Mean Absolute Error(MAE)
+		# Mean Absolute Percentage Error(MAPE)
+                    
+use classicmodels;
+select * from payments;
+select year(paymentDate), month(paymentDate), sum(amount) from payments group by 1, 2 order by 1, 2;
+
+with
+cte1 as(select year(paymentDate) YR, month(paymentDate) MO, sum(amount) ACTUAL from payments group by 1, 2 order by 1, 2),
+cte2 as(select cte1.*, lag(actual) over (order by YR, MO) forecast from cte1 group by 1, 2 order by 1, 2)
+select cte2.*, 
+
+sqrt(avg(power((actual - forecast), 2))) RMSE,
+avg(abs((actual - forecast))) MAE,
+avg(abs(actual - forecast) / abs(actual)) * 100 MAPE
+
+from cte2 group by 1, 2 order by 1, 2;
