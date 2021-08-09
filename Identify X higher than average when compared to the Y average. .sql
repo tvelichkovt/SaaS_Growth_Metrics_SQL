aff9a799866743c6ae8eabe5040851be6e00fd90
Todor <tvelichkovt@gmail.com>
2021-08-09 21:15:16 +0300

    SaaS_Growth_Metrics_SQL

--- Identify X higher than average when compared to the Y average. .sql
+++ Identify X higher than average when compared to the Y average. .sql
@@ -1,4 +1,4 @@
-# Write a query that identifies cities with higher than average home prices when compared to the national average. 
+# Identify cities with higher than average home prices when compared to the national average. 
 	# Output the city names.
 
 use classicmodels;
